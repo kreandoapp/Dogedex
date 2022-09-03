853f67c872247ff361f519cad2df9e1f87379453
Matias Diaz <ma.diaz@redlink.com.ar>
2022-09-03 18:36:07 -0300

    git cambios 3/9/2022 18:35

--- app/src/main/java/com/example/dogedex/api/responses/DogListApiResponse.kt
+++ app/src/main/java/com/example/dogedex/api/responses/DogListApiResponse.kt
@@ -5,4 +5,4 @@ import com.squareup.moshi.Json
 class DogListApiResponse (
     val message : String,
     @field:Json(name = "is_success") val isSuccess : Boolean,
-    val data : UserResponse)
+    val data : DogListResponse)
--- app/src/main/java/com/example/dogedex/auth/LoginActivity.kt
+++ app/src/main/java/com/example/dogedex/auth/LoginActivity.kt
@@ -1,10 +1,17 @@
 package com.example.dogedex.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import com.example.dogedex.MainActivity
 import com.example.dogedex.R
+import com.example.dogedex.api.ApiResponseStatus
 import com.example.dogedex.databinding.ActivityLoginBinding
 import com.example.dogedex.databinding.ActivityMainBinding
 
@@ -12,12 +19,44 @@ class LoginActivity : AppCompatActivity(),LoginFragment.LoginFragmentActions ,Si
 
    private val viewModel : AuthViewModel  by viewModels()
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel.status.observe(this) { status ->
+            when (status) {
+                is ApiResponseStatus.Error -> {
+                    binding.loadingWheel.visibility = View.GONE
+                    showErrorDialog(status.messageId)
+                }
+                is ApiResponseStatus.Loading -> binding.loadingWheel.visibility = View.VISIBLE
+                is ApiResponseStatus.Success -> binding.loadingWheel.visibility = View.GONE
+            }
+        }
+        viewModel.user.observe(this){
+            if(it != null){
+                startMainactivity()
+            }
+        }
 
+    }
+
+    private fun startMainactivity() {
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+
+    private fun showErrorDialog(messageId:Int){
+            AlertDialog.Builder(this)
+                .setTitle("There one error")
+                .setMessage(messageId)
+                .setPositiveButton(android.R.string.ok){ _,_->{}
+
+                }
+                .create()
+                .show()
     }
 
     override fun onregisterButonClick() {
@@ -30,6 +69,6 @@ class LoginActivity : AppCompatActivity(),LoginFragment.LoginFragmentActions ,Si
         password: String,
         passwordConfirmation: String
     ) {
-        //TODO
+       viewModel.signup(email,password,passwordConfirmation)
     }
 }
